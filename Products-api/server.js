aaea91d2466e1ae7d4f0f3b97b6583c886d424c4
Subrat090 <subratmahapatro09@gmail.com>
2025-03-01 04:26:34 +0530

    port edited

--- Products-api/server.js
+++ Products-api/server.js
@@ -17,5 +17,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
